58f7818d34e72fe2ebf9418ac2bc106d2290d49f
nrakochy <nick.rakochy@gmail.com>
2014-12-31 15:38:30 -0600

    Search form autocomplete functional

--- public/javascripts/maps.js
+++ public/javascripts/maps.js
@@ -1,10 +1,17 @@
+var initialize = function(graffitiData){
+  createFormInputAutocomplete();
+  chicagoMap = drawChicagoMap();
+  addMapMarkers(graffitiData, chicagoMap);
+}
+
 var drawChicagoMap = function drawMap(){
   chicagoCoordinates = new google.maps.LatLng(41.8789, -87.6358);
   mapOptions = {
-    zoom: 12,
+    zoom: 11,
     center: chicagoCoordinates
   }
-  return new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  baseMap = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  return baseMap;
 }
 
 var addMapMarkers = function addMarkers(chicagoData, map){
@@ -14,39 +21,47 @@ var addMapMarkers = function addMarkers(chicagoData, map){
 };
 
 var createMapMarker = function createMarker(map, dataRecord){
-  markerLatLng = new google.maps.LatLng(dataRecord['latitude'],dataRecord['longitude']);
+  myLatLng = new google.maps.LatLng(dataRecord['latitude'],dataRecord['longitude']);
   newMarkerLocationInformation = setLocationInformation(dataRecord);
   dataWindow = new google.maps.InfoWindow({
     content: ""
   })
 
   newMapMarker = new google.maps.Marker({
-    position: markerLatLng,
+    position: myLatLng,
     clickable: true,
     icon: '../style/red-marker.png',
     map: map
   });
 
-  bindInfoWindow(map, newMapMarker, dataWindow, newMarkerLocationInformation);
+  bindInfoWindowData(map, newMapMarker, dataWindow, newMarkerLocationInformation);
 };
 
-var bindInfoWindow = function bindWindow(map, marker, windowObj, windowData){
+var bindInfoWindowData = function bindWindowData(map, marker, windowObj, windowData){
   google.maps.event.addListener(marker, 'click', function googleClickListener() {
     windowObj.setContent(windowData);
     windowObj.open(map, marker)
   });
-
-
 }
 
 var setLocationInformation = function setInformation(dataRecord){
-  locationInformation = $('<div class="mapInfoWindow">'+
-    '<h4>' + dataRecord['street_address'] + '</h4>'+
-        '<h5>Days Open:  '+ dataRecord['days_elapsed_since_request'] + '</h5>' +
-        '<h5>Ward:  '+ dataRecord['ward'] + '</h5>' +
-        '<h5>Zip Code:  '+ dataRecord['zip_code'] + '</h5>' +
-  '</div>')
+  locationInformation =
+    $('<div class="mapInfoWindow">'+
+      '<h4>'             + dataRecord['street_address']             + '</h4>' +
+      '<h5>Days Open:  ' + dataRecord['days_elapsed_since_request'] + '</h5>' +
+      '<h5>Ward:  '      + dataRecord['ward']                       + '</h5>' +
+      '<h5>Zip Code:  '  + dataRecord['zip_code']                   + '</h5>' +
+      '</div>')
   return locationInformation[0];
 }
 
-
+var createFormInputAutocomplete = function createAutocomplete(){
+  searchInput = (document.getElementById('searchLocation'));
+  searchAutoComplete = new google.maps.places.Autocomplete(searchInput);
+  google.maps.event.addListener(searchAutoComplete, 'place_changed', function (){
+    searchAutoComplete.getPlace();
+  });
+  google.maps.event.addListener(searchInput, 'click', function(){
+    searchInput.value = '';
+  });
+};
