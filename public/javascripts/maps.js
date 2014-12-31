d65c99c5a91633a2de06cc3c5feec7411ad5a89e
nrakochy <nick.rakochy@gmail.com>
2014-12-31 16:56:45 -0600

    Added biasSearch results method. Map bounds not setting correctly, so not yet functional

--- public/javascripts/maps.js
+++ public/javascripts/maps.js
@@ -1,26 +1,34 @@
 var initialize = function(graffitiData){
-  createFormInputAutocomplete();
+  searchBox = createInputSearchBox();
   chicagoMap = drawChicagoMap();
   addMapMarkers(graffitiData, chicagoMap);
+  biasSearchBoxResults(searchBox, chicagoMap);
 }
 
-var drawChicagoMap = function drawMap(){
-  chicagoCoordinates = new google.maps.LatLng(41.8789, -87.6358);
+function drawChicagoMap(){
+
+  southwestCornerChicago = new google.maps.LatLng(41.6072, -87.8617);
+  northeastCornerChicago = new google.maps.LatLng(42.106111, -87.471111);
+  defaultBounds = new google.maps.LatLngBounds(southwestCornerChicago, northeastCornerChicago);
+  chicagoCenterCoordinates = new google.maps.LatLng(41.8789, -87.6358);
+
   mapOptions = {
-    zoom: 11,
-    center: chicagoCoordinates
+    zoom: 10,
+    center: chicagoCenterCoordinates
   }
   baseMap = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  baseMap.fitBounds(defaultBounds);
+  console.log(baseMap.getBounds());
   return baseMap;
 }
 
-var addMapMarkers = function addMarkers(chicagoData, map){
+function addMapMarkers(chicagoData, map){
   $.each(chicagoData, function addMarkersForEach(index, dataRecord){
     createMapMarker(map, dataRecord);
   });
 };
 
-var createMapMarker = function createMarker(map, dataRecord){
+function createMapMarker(map, dataRecord){
   myLatLng = new google.maps.LatLng(dataRecord['latitude'],dataRecord['longitude']);
   newMarkerLocationInformation = setLocationInformation(dataRecord);
   dataWindow = new google.maps.InfoWindow({
@@ -37,14 +45,14 @@ var createMapMarker = function createMarker(map, dataRecord){
   bindInfoWindowData(map, newMapMarker, dataWindow, newMarkerLocationInformation);
 };
 
-var bindInfoWindowData = function bindWindowData(map, marker, windowObj, windowData){
+function bindInfoWindowData(map, marker, windowObj, windowData){
   google.maps.event.addListener(marker, 'click', function googleClickListener() {
     windowObj.setContent(windowData);
     windowObj.open(map, marker)
   });
 }
 
-var setLocationInformation = function setInformation(dataRecord){
+function setLocationInformation(dataRecord){
   locationInformation =
     $('<div class="mapInfoWindow">'+
       '<h4>'             + dataRecord['street_address']             + '</h4>' +
@@ -55,13 +63,19 @@ var setLocationInformation = function setInformation(dataRecord){
   return locationInformation[0];
 }
 
-var createFormInputAutocomplete = function createAutocomplete(){
+function createInputSearchBox(){
   searchInput = (document.getElementById('searchLocation'));
-  searchAutoComplete = new google.maps.places.Autocomplete(searchInput);
-  google.maps.event.addListener(searchAutoComplete, 'place_changed', function (){
-    searchAutoComplete.getPlace();
+  searchBox = new google.maps.places.SearchBox(searchInput);
+  google.maps.event.addListener(searchBox, 'place_changed', function (){
+    searchBox.getPlace();
   });
   google.maps.event.addListener(searchInput, 'click', function(){
     searchInput.value = '';
   });
+  return searchBox;
 };
+
+function biasSearchBoxResults(inputSearchBox, map){
+    bounds = map.getBounds();
+    inputSearchBox.setBounds(bounds);
+}
