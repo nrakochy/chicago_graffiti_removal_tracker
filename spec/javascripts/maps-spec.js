0864f0ea3da08af86d3391198feb2241af8a1870
nrakochy <nick.rakochy@gmail.com>
2015-01-06 13:44:38 -0600

    Added Sinon for better spy objects

--- spec/javascripts/maps-spec.js
+++ spec/javascripts/maps-spec.js
@@ -1,16 +1,22 @@
 describe("mapJs", function(){
-  describe("GoogleMapsMock", function(){
-    var googleMap;
+  var googleMap;
+  beforeEach(function(){
+    googleMap = GoogleMapsMock;
+  });
 
-    beforeEach(function(){
-      googleMap = GoogleMapsMock;
-    });
-
+  describe("GoogleMapsMock helper is wired up to the spec", function(){
     it("creates a googleMap", function() {
       expect(googleMap).not.toBe(undefined);
     })
-
   });
+
+  describe("drawChicagoMap", function(){
+    it("should call the Google map constructor", function(){
+      newMap = new drawChicagoMap();
+      expect(newMap).toBeDefined();
+    })
+  });
+
 });
 
 
