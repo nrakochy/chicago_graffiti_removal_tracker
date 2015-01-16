f7d8d8afce051bf12656aeb26fc5f57e7b8359fc
nrakochy <nick.rakochy@gmail.com>
2015-01-16 17:12:21 -0600

    Moved external API call from initialization to Sinatra route. Moved click event from filterSearch to index erb call.

--- public/javascripts/filterSearch.js
+++ public/javascripts/filterSearch.js
@@ -1,44 +1,39 @@
-var filterSearchWithUserInputAndLinkToChicagoWebsite = function filterSearch(map, locationMarkers){
-  var searchForm = $('[id=searchForm]');
-  searchForm.submit(function( event ) {
-    event.preventDefault();
-    searchRequest = $('[data-name=search-bar]').val();
+var filterSearchWithUserInputAndLinkToChicagoWebsite = function filterSearch(map, locationMarkers, searchRequest){
     centerMapOnUserInput(searchRequest, map);
     linkToChicagoWebsite();
     resetSearchBar();
     setMapMarkers(map, locationMarkers);
-  });
-}
+};
 
-
-function centerMapOnUserInput(searchRequest, map){
-  var geocoder = new google.maps.Geocoder();
-  geocoder.geocode({"address": searchRequest}, function(result, status) {
-    if (status == google.maps.GeocoderStatus.OK) {
-      var newLocation = result[0].geometry.location;
-      map.setZoom(14);
-      map.panTo(newLocation);
-    } else {
-      map.setZoom(14);
-      console.log('The call to Google api went wrong. Here is the reason: ' + status);
-    };
-  });
-}
-
-function setMapMarkers(map, mapMarkers){
-  google.maps.event.addListener(map, 'idle', function(){
-    $.each(mapMarkers, function setMap(index, marker){
-      marker.setMap(map);
+  function centerMapOnUserInput(searchRequest, map){
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({"address": searchRequest}, function(result, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        var newLocation = result[0].geometry.location;
+        map.setZoom(14);
+        map.panTo(newLocation);
+      } else {
+        map.setZoom(14);
+        console.log('The call to Google api went wrong. Here is the reason: ' + status);
+      };
     });
-  })
-}
+  }
 
-function linkToChicagoWebsite() {
-  var link = "<h2>You can make a new graffiti removal request on the <a href='http://311request.cityofchicago.org/reports/new?service_id=4fd3b167e750846744000005' target='_blank'>Chicago 311 website</a></h2>"
-  var chicagoLink = $($.parseHTML(link))
-  $('h1').replaceWith(chicagoLink);
-}
+  function setMapMarkers(map, mapMarkers){
+    google.maps.event.addListener(map, 'idle', function(){
+      $.each(mapMarkers, function setMap(index, marker){
+        marker.setMap(map);
+      });
+    })
+  }
 
-function resetSearchBar(){
-  $('[value]').val("");
-}
+  function linkToChicagoWebsite() {
+    var link = "<h2>You can make a new graffiti removal request on the <a href='http://311request.cityofchicago.org/reports/new?service_id=4fd3b167e750846744000005' target='_blank'>Chicago 311 website</a></h2>"
+    var chicagoLink = $($.parseHTML(link))
+    $('h1').replaceWith(chicagoLink);
+  }
+
+  function resetSearchBar(){
+    $('[value]').val("");
+  }
+
