238bab066ad8d198f399582b034d3ec1ff384c3b
nrakochy <nick.rakochy@gmail.com>
2015-01-19 23:55:53 -0600

    Unicorn configuration change in app.rb to compile assets. Take 3.0

--- app.rb
+++ app.rb
@@ -34,8 +34,8 @@ class GraffitiRemovalTracker < Sinatra::Base
 
 configure :production do
     assets {
-        serve '/js',  from: 'current/public/js'
-        serve '/css', from: 'current/public/css'
+        serve '/js',  from: './js'
+        serve '/css', from: './css'
     }
 end
 
